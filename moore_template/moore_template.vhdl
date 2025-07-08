967178309bed7160741014da981d012a284b21bb
Martin Peth <martin@peth.pl>
2025-07-09 00:28:51 +0200

    feat(moore_template): added moore template

---
+++ moore_template/moore_template.vhdl
@@ -0,0 +1,57 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity moore_template is
+    port(
+        clk: in std_logic;
+        reset: in std_logic;
+        enable: in std_logic;
+        d: in std_logic;
+        q: out std_logic
+    );
+end entity;
+
+architecture behave of moore_template is
+    type fsm_state is (S0, S1);
+    signal current_state, next_state: fsm_state;
+
+begin
+    STATE_MEMORY: process(clk, reset)
+    begin
+        if reset = '1' then
+            current_state <= S0;
+        elsif rising_edge(clk) and enable = '1' then
+            current_state <= next_state;
+        end if;
+    end process STATE_MEMORY;
+
+    TRANSITION_LOGIC: process(current_state, d)
+    begin
+        case current_state is
+            when S0 => 
+                if d = '0' then
+                   next_state <= S1; 
+                end if;
+            when S1 => 
+                if d = '1' then
+                    next_state <= S0;
+                end if;
+            when others => 
+                next_state <= S0;
+        end case;
+    end process TRANSITION_LOGIC;
+
+    OUTPUT_LOGIC: process(current_state)
+    begin
+        case current_state is
+            when S0 => 
+                q <= '0';
+            when S1 => 
+                q <= '1';
+            when others =>
+                q <= '0';
+        end case;
+    end process OUTPUT_LOGIC;
+
+end behave;
---
+++ moore_template/moore_template_tb.vhdl
@@ -0,0 +1,53 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity moore_template_tb is
+end entity;
+
+architecture testbench of moore_template_tb is
+    component moore_template
+        port(
+            clk: in std_logic;
+            reset: in std_logic;
+            enable: in std_logic;
+            d: in std_logic;
+            q: out std_logic
+        );
+    end component;
+
+    constant CLK_PERIOD: time := 20 ns;
+    signal s_clk, s_reset, s_enable, s_d, s_q: std_logic; 
+    signal is_finished: boolean := false;
+
+begin
+    DUT: moore_template port map(s_clk, s_reset, s_enable, s_d, s_q);
+
+    CLK_PROCESS: process
+    begin
+        while not is_finished loop
+            s_clk <= '0';
+            wait for CLK_PERIOD/2;
+            s_clk <= '1';
+            wait for CLK_PERIOD/2;
+        end loop;
+        wait;
+    end process CLK_PROCESS;
+
+    TEST_PROCESS: process
+    begin
+        s_reset <= '1';
+        wait for CLK_PERIOD/2;
+
+        s_reset <= '0';
+        s_d <= '0';
+        wait for CLK_PERIOD/2;
+        
+        s_enable <= '1';
+
+        is_finished <= true;
+        assert false report "Reached end of test";
+        wait;
+    end process;
+
+end testbench ; -- testbench
